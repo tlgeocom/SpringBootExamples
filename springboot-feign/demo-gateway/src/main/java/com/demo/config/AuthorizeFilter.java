33763bedca24f6cbcf4e29275fa9150b73556665
George <123@gmail.com>
2023-12-12 11:38:13 +0800

    Customize Gateway filter implementing from AbstractGatewayFilterFactory to realize url dynamic forward

--- springboot-feign/demo-gateway/src/main/java/com/demo/config/AuthorizeFilter.java
+++ springboot-feign/demo-gateway/src/main/java/com/demo/config/AuthorizeFilter.java
@@ -1,3 +1,4 @@
+/*
 package com.demo.config;
 
 
@@ -33,3 +34,4 @@ public class AuthorizeFilter implements GlobalFilter {
 
     }
 }
+*/
---
+++ springboot-feign/demo-gateway/src/main/java/com/demo/config/UriHostPlaceholderFilter.java
@@ -0,0 +1,75 @@
+package com.demo.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+@Slf4j
+@Component
+public class UriHostPlaceholderFilter extends AbstractGatewayFilterFactory<UriHostPlaceholderFilter.Config> {
+
+
+    public UriHostPlaceholderFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Collections.singletonList("order");
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return new OrderedGatewayFilter((exchange, chain) -> {
+            String newurl = "";
+            //获取请求参数
+            ServerHttpRequest request = exchange.getRequest();
+            MultiValueMap<String, String> queryParams = request.getQueryParams();
+
+            String type = queryParams.getFirst("type");
+            if(!type.equals("")){
+                newurl = "http://192.168.142.177:2183/geoserver/gwc/service/wmts?layer=ellip%3Alangfang_img_c_1_16_big&style=&tilematrixset=EPSG%3A4326&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=EPSG%3A4326%3A8&TileCol=420&TileRow=71";
+            }
+            if (!newurl.equals("")) {
+
+                URI newUri = null;
+                try {
+                    newUri = new URI(newurl);
+                } catch (URISyntaxException e) {
+                    log.error("uri error", e);
+
+                }
+
+                exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, newUri);
+            }
+            return chain.filter(exchange);
+        }, config.getOrder());
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class Config {
+        private int order;
+
+        public Config(int order) {
+            this.order = order;
+        }
+    }
+}
+
